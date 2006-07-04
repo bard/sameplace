c703665a4dc53a412ea4f672b52faa378306aea1
Massimiliano Mirra <bard@hyperstruct.net>
2006-07-04 12:07:45 +0200

    Room topic is displayed and can be changed.
    darcs-hash:20060704100745-8cc29-1f1e3def366f246bb49e4347605aa6c5c1d3736c.gz

--- chrome/content/mozeskine.js
+++ chrome/content/mozeskine.js
@@ -13,7 +13,7 @@ var observerService = Components
 // ----------------------------------------------------------------------
 // GLOBAL STATE
 
-var client, userJid, roomAddress, roomNick;
+var client, userJid, roomAddress, roomNick, roomTopic = '';
 
 
 // ----------------------------------------------------------------------
@@ -27,9 +27,19 @@ function init(event) {
         .addEventListener('keypress', pressedKeyInChatInput, false);
     _('chat-output').contentDocument
         .addEventListener('click', clickedSaveButton, false);
+    _('chat-output').contentDocument
+        .addEventListener('click', clickedHeader, false);
     _('notes').contentDocument
         .addEventListener('click', clickedRemoveButton, false);
 
+    _('chat-output').contentWindow.addEventListener(
+        'resize', function(event) {
+            setCroppedContent(
+                _('chat-output').contentDocument.getElementById('topic'),
+                roomTopic);
+        }, false);
+        
+    
     client = new Client();
     client.on(
         {tag: 'data'}, function(data) {
@@ -44,6 +54,10 @@ function init(event) {
                 return s.moz::x.toXMLString();
             }}, function(message) { receiveAction(message); });
     client.on(
+        {tag: 'message', direction: 'in', stanza: function(s) {
+                return s.@type == 'groupchat' && s.subject.toString();
+            }}, function(message) { receiveRoomTopic(message); });
+    client.on(
         {tag: 'presence', direction: 'in', stanza: function(s) {
                 return s.muc::x.toXMLString();
             }}, function(presence) { receivePresence(presence) });
@@ -55,7 +69,17 @@ function finish() {
 
 
 // ----------------------------------------------------------------------
-// GUI ACTIONS
+// GUI UTILITIES
+
+function setCroppedContent(element, content) {
+    element.textContent = content;
+    var containerWidth = element.parentNode.offsetWidth;
+
+    while(element.offsetWidth > containerWidth) {
+        content = content.substring(0, content.length-1);
+        element.textContent = content + '\u2026';
+    }
+} 
 
 function _(id) {
     return document.getElementById(id);
@@ -75,6 +99,10 @@ function cloneBlueprint(name) {
         .getElementsByAttribute('role', name)[0]
         .cloneNode(true);
 }
+
+
+// ----------------------------------------------------------------------
+// GUI ACTIONS
 
 function jabberDebug(text) {
     var debugLine = cloneBlueprint('debug-line');
@@ -110,6 +138,14 @@ function displayChatMessage(from, content) {
 
     doc.getElementById('messages').appendChild(item);
     _('chat-output').contentWindow.scrollTo(0, doc.height);
+}
+
+function displayRoomTopic(content) {
+    roomTopic = content;
+
+    setCroppedContent(
+        _('chat-output').contentDocument.getElementById('topic'),
+        roomTopic);
 }
 
 function displayEvent(content, additionalClass) {
@@ -178,6 +214,23 @@ function clickedSaveButton(event) {
 
     if(child)
         sendNoteAddition(child.textContent);
+}
+
+function clickedHeader(event) {
+    if(event.target.id != 'header' && event.target.id != 'topic')
+        return;
+
+    var input = { value: '' };
+    var check = { value: false };
+    if(Components
+       .classes["@mozilla.org/embedcomp/prompt-service;1"]
+       .getService(Components.interfaces.nsIPromptService)
+       .prompt(null, 'Mozeskine', 'Set topic for this room:', input, null, check))
+        client.send(
+            userJid,
+            <message to={roomAddress} type="groupchat">
+            <subject>{input.value}</subject>
+            </message>);
 }
 
 function clickedRemoveButton(event) {
@@ -278,6 +331,13 @@ function receiveChatMessage(message) {
         null, 'im-incoming', message.stanza.toString());
 }
 
+function receiveRoomTopic(message) {
+    var nick = message.stanza.@from.toString().match(/\/(.+)$/)[1];
+    
+    displayEvent(nick + ' set the topic to "' + message.stanza.subject + '"', 'topic');
+    displayRoomTopic(message.stanza.subject.toString());
+}
+
 function receivePresence(presence) {
     var nick = presence.stanza.@from.toString().match(/\/(.+)$/)[1];
     var item = _('participants').getElementsByAttribute('label', nick)[0];
@@ -297,7 +357,7 @@ function receivePresence(presence) {
             break;
         default:
             _('participants').appendItem(nick);
-            displayEvent(nick + ' joined the room', 'join');
+            displayEvent(nick + ' entered the room', 'join');
         }
     }
 }
