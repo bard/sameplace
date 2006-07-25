429ba4a1801679d7db4e70e4e05d02ad92e61f4a
Massimiliano Mirra <bard@hyperstruct.net>
2006-07-25 22:14:19 +0200

    Using attach() to pass account and address parameters to SamePage.
    darcs-hash:20060725201419-8cc29-bc7df46dc1050c2aa87be64e5ff3e73dc0cbd89f.gz

--- chrome/content/plugin/samepage.js
+++ chrome/content/plugin/samepage.js
@@ -4,12 +4,13 @@
 function samepageRequestedOpenEditor(event) {
     var account = getAncestorAttribute(event.currentTarget, 'account');
     var address = getAncestorAttribute(event.currentTarget, 'address');
-    var url = 'chrome://samepage/content/samepage.xul' + '?account=' + account + '&address=' + address;
+    var url = 'chrome://samepage/content/samepage.xul';
 
     withContent(
         account, address, url,
         function(window) {
             var index = findBrowserIndex(account, address, url);
             top.getBrowser().selectedTab = top.getBrowser().tabContainer.childNodes[index];
+            window.attach(account, address);
         });
 }
