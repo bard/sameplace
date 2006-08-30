3d178cef6520fd99a4e3be76f2ce8291a44fbfcb
Massimiliano Mirra <bard@hyperstruct.net>
2006-08-30 19:15:22 +0200

    Brought in sidebar code previously in xmpp4moz.
    darcs-hash:20060830171522-8cc29-ed55d5daa3f8ccc17398115436733566e43fe7fc.gz

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -3,14 +3,32 @@ var mozeskine_xmppChannel = XMPP.createChannel();
 
 mozeskine_xmppChannel.on(
     {event: 'stream', direction: 'out', state: 'open'},
-    function(stream) { mozeskine_loadLivebar(); });
+    function(stream) { mozeskine_loadSidebar(); });
 
-function mozeskine_loadLivebar(force) {
-    var livebar = document.getElementById('livebar');
-    var frame = livebar.firstChild.contentWindow;
+function mozeskine_loadSidebar(force) {
+    var sidebar = document.getElementById('mozeskine-sidebar');
+    var frame = sidebar.firstChild.contentWindow;
 
     if(force || frame.location.href != 'chrome://mozeskine/content/mozeskine.xul') 
         frame.location.href = 'chrome://mozeskine/content/mozeskine.xul';
 
-    xmppShowLivebar();
+    mozeskine_showSidebar();
+}
+
+function mozeskine_toggleSidebar() {
+    var sidebar = document.getElementById('mozeskine-sidebar');
+    var splitter = document.getElementById('mozeskine-sidebar-splitter');
+
+    if(sidebar.collapsed) {
+        sidebar.collapsed = false;
+        splitter.hidden = false;
+    } else {
+        sidebar.collapsed = true;
+        splitter.hidden = true;
+    }
+}
+
+function mozeskine_showSidebar() {
+    document.getElementById('mozeskine-sidebar').collapsed = false;
+    document.getElementById('mozeskine-sidebar-splitter').hidden = false;
 }
