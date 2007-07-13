3a9e63150ff405b76248673a2579a912430739dc
Massimiliano Mirra <bard@hyperstruct.net>
2007-07-13 17:34:36 +0200

    Preferences: bad default for toggleSidebarKey.  Fixed.
    darcs-hash:20070713153436-8cc29-7d3e9a048f0b6991c68f06d96f40dcb6cf08d294.gz

--- defaults/preferences/sameplace.js
+++ defaults/preferences/sameplace.js
@@ -2,4 +2,5 @@ pref("extensions.sameplace.defaultAppUrl", "chrome://sameplace/content/app/chat.
 pref("extensions.sameplace.getAttentionOnMessage", true);
 pref("extensions.sameplace.version", "");
 pref("extensions.sameplace.scriptlets.enabled", "[]");
-pref("extensions.sameplace.toggleSidebarKey", "({keyCodeName:'DOM_VK_F12'})");
+pref("extensions.sameplace.toggleSidebarKey",
+     "({ctrlKey:false, metaKey:false, shiftKey:false, altKey:false, charCode:0, keyCodeName:\"DOM_VK_F12\"})");
