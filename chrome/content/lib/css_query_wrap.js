004c53b3407680066dee82997ed11c4d36e00a25
Massimiliano Mirra <bard@hyperstruct.net>
2007-09-17 11:23:30 +0200

    CSS Query: rearranged source into interface/implementation files.
    darcs-hash:20070917092330-8cc29-40e1800631ef1aa32b2681400138cfd1d98788fa.gz

--- chrome/content/lib/css_query_wrap.js
+++
@@ -1,9 +0,0 @@
-function $() {
-    return arguments.callee.impl.$.apply(null, arguments);
-}
-$.impl = {};
-
-Components
-.classes['@mozilla.org/moz/jssubscript-loader;1']
-.getService(Components.interfaces.mozIJSSubScriptLoader)
-    .loadSubScript('chrome://sameplace/content/lib/css_query.js', $.impl);
