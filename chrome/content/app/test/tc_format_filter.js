44992ecdd271c84bdc57529e3f60077088284e30
Massimiliano Mirra <bard@hyperstruct.net>
2007-11-06 16:17:17 +0100

    Chat app: added format filter tests.
    darcs-hash:20071106151717-8cc29-6e7c1cb090a3a5e6a22391d61316ddb08e585fe4.gz

---
+++ chrome/content/app/test/tc_format_filter.js
@@ -0,0 +1,42 @@
+var regexp = /(^|\s)\*(.+?)\*($|[^\d\w])/g;
+
+var shouldMatch = [
+    ' *hello* ',
+    ' *hello world* ',
+    '\n*hello world*',
+    '*hello world* ',
+    '*hello world*',
+    '*hello world*,'
+];
+
+var shouldNotMatch = [
+    'http://test.com/my*nice*url',
+    'inner*stars*stars'
+];
+
+var matchResults = shouldMatch.map(function(string) {
+    return string + '\t->\t' + (string.match(regexp) ? 'OK' : 'FAIL');
+});
+
+var notMatchResults = shouldNotMatch.map(function(string) {
+    return string + '\t->\t' + (string.match(regexp) ? 'FAIL' : 'OK');
+});
+
+
+repl.print(matchResults.concat(notMatchResults).join('\n'));
+
+
+    function processFormatBold(xmlMessageBody) {
+        var regexp = /(^|\s)\*(.+?)\*($|[^\w\d])/g;
+        
+        return xml.mapTextNodes(xmlMessageBody, function(textNode) {
+            return text.mapMatch(textNode.toString(), regexp, function(wholeMatch, before,
+                                                                content, after) {
+                return <span style="font-weight: bold;">{before}{content}{after}</span>;
+            });
+        });
+    }
+
+XML.ignoreWhitespace = false;
+XML.prettyPrinting = false;
+processFormatBold(<body> hello *world* ahah</body>)
