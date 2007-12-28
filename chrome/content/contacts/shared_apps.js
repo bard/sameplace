f8bdf97d78d945071c243f55d054c8dbabcf816b
Massimiliano Mirra <bard@hyperstruct.net>
2007-12-28 01:54:52 +0100

    New UI: updated.
    darcs-hash:20071228005452-8cc29-1b2605a52b333fe58531e4d92e13725fc024ed12.gz

---
+++ chrome/content/contacts/shared_apps.js
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2006-2007 by Massimiliano Mirra
+ * 
+ * This file is part of SamePlace.
+ * 
+ * SamePlace is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * SamePlace is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Author: Massimiliano Mirra, <bard [at] hyperstruct [dot] net>
+ *  
+ */
+
+
+// GUI REACTIONS
+// ----------------------------------------------------------------------
+
+window.addEventListener('load', function(event) {
+    var xulContactPopup = $('#contact-popup');
+    fetchFeed('http://apps.sameplace.cc/feed.xml', function(feed, e) {
+        if(!feed) throw e;
+        
+        var menus = {};
+        function menuFor(category) {
+            if(!menus[category]) {
+                var xulMenu = document.createElement('menu');
+                xulMenu.setAttribute('class', 'offset');
+                xulMenu.setAttribute('label', category);
+                xulMenu.setAttribute('tooltiptext', category);
+                xulContactPopup.insertBefore(
+                    xulMenu,
+                    $(xulContactPopup, '> .shared-app-separator'));
+                
+                var xulPopup = document.createElement('menupopup');
+                xulMenu.appendChild(xulPopup);
+                menus[category] = xulPopup;
+            }
+            return menus[category];
+        }
+        
+        for(var i=0; i<feed.items.length; i++) {
+            var item = feed.items.queryElementAt(i, Ci.nsIFeedEntry);
+            
+            var menuItem = document.createElement('menuitem');
+            menuItem.setAttribute('label', item.fields.getProperty('title'));
+            menuItem.setAttribute('value', item.fields.getProperty('link'));
+            menuItem.setAttribute('tooltiptext', item.fields.getProperty('description'));
+            menuFor(item.fields.getProperty('dc:subject')).appendChild(menuItem);
+        }
+    });
+}, false);
+
+
+// NETWORK UTILITIES
+// ----------------------------------------------------------------------
+
+function fetchFeed(feedUrl, continuation) {
+    if(!Ci.nsIFeed) {
+        continuation(null);
+        return;
+    }
+    
+    var req = new XMLHttpRequest();
+
+    req.onload = function() {
+        var data = req.responseText;
+
+        var ioService = Cc['@mozilla.org/network/io-service;1']
+        .getService(Ci.nsIIOService);
+        var uri = ioService.newURI(feedUrl, null, null);
+
+        if(data.length) {
+            var parser = Cc['@mozilla.org/feed-processor;1']
+                .createInstance(Ci.nsIFeedProcessor);
+            try {
+                parser.listener = {
+                    handleResult: function(result) {
+                        continuation(result.doc.QueryInterface(Ci.nsIFeed));
+                    }
+                };
+                parser.parseFromString(data, uri);
+            }
+            catch(e) {
+                continuation(null, e);
+            }
+        }
+    };
+
+    req.open('GET', feedUrl, true);
+    try {
+        req.send(null);
+    } catch(e) {
+        continuation(null, e);
+    }
+}
