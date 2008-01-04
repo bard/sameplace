01ec4c6a17d5d505c5fc21afd4d8113cd7bd4dc6
Massimiliano Mirra <bard@hyperstruct.net>
2008-01-04 15:47:44 +0100

    New UI: updated.
    darcs-hash:20080104144744-8cc29-7123fffb88140118bb5e6fe855282d9d0c5b3651.gz

---
+++ chrome/content/experimental/conversations_overlay_outer_impl.js
@@ -0,0 +1,160 @@
+/*
+ * Copyright 2006-2007 by Massimiliano Mirra
+ * 
+ * This file is part of SamePlace.
+ * 
+ * SamePlace is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * SamePlace is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * The interactive user interfaces in modified source and object code
+ * versions of this program must display Appropriate Legal Notices, as
+ * required under Section 5 of the GNU General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU General Public License
+ * version 3, modified versions must display the "Powered by SamePlace"
+ * logo to users in a legible manner and the GPLv3 text must be made
+ * available to them.
+ * 
+ * Author: Massimiliano Mirra, <bard [at] hyperstruct [dot] net>
+ *  
+ */
+
+
+// DEFINITIONS
+// ----------------------------------------------------------------------
+
+
+// STATE
+// ----------------------------------------------------------------------
+
+var channel;
+
+
+// INITIALIZATION/FINALIZATION
+// ----------------------------------------------------------------------
+
+function init() {
+    window.addEventListener('contact/select', selectedContact, false);
+
+    /*
+    window.addEventListener('conversation/open', openedConversation, false);
+    
+    window.addEventListener('conversation/close', closedConversation, false);
+    
+    delayedMouseOver($('.scroll-arrow-down'), function() {
+        addClass($('#contacts-stack'), 'attention-on-contacts');
+    });
+
+    delayedMouseOver($('.scroll-arrow-up'), function() {
+        addClass($('#contacts-stack'), 'attention-on-contacts');
+    });
+
+    delayedMouseOver($('#conversations'), function() {
+        removeClass($('#contacts-stack'), 'attention-on-contacts');
+    });
+
+    $('#conversations').addEventListener('click', clickedInConversation, false);
+*/
+    channel = XMPP.createChannel();
+/*
+    channel.on({
+        event     : 'message',
+        direction : 'out',
+        stanza    : function(s) {
+            return s.ns_chatstates::active != undefined;
+        }
+    }, sentChatActivation);
+
+    $('#conversations').contentDocument.location.href = 'conversations.xul';
+*/
+}
+
+function finish() {
+    channel.release();
+}
+
+
+// GUI REACTIONS
+// ----------------------------------------------------------------------
+
+function selectedContact(event) {
+    var account = event.target.getAttribute('account');
+    var address = event.target.getAttribute('address');
+    var convWindow =
+        Cc['@mozilla.org/appshell/window-mediator;1']
+        .getService(Ci.nsIWindowMediator)
+        .getMostRecentWindow('SamePlace:Conversations') ||
+        window.open('chrome://sameplace/content/experimental/conversations.xul',
+                    'SamePlace:Conversations', 'chrome');
+
+    if(convWindow.document.location.href == 'about:blank')
+        convWindow.addEventListener('load', function(event) {
+            convWindow.selectedContact(account, address);
+        }, false);
+    else
+        convWindow.selectedContact(account, address);
+}
+
+// function openedConversation(event) {
+//     addClass($('#conversations-box'), 'expanded');
+//     addClass($('#conversations').contentDocument.documentElement, 'expanded');
+// }
+
+// function closedConversation(event) {
+//     if($('#conversations').contentWindow.getCount() == 0) {
+//         removeClass($('#conversations-box'), 'expanded');
+//         removeClass($('#conversations').contentDocument.documentElement, 'expanded');
+//     }
+// }
+
+// function clickedInConversation(event) {
+//     var htmlAnchor =
+//         event.target instanceof HTMLAnchorElement ?
+//         event.target :
+//         (event.target.parentNode instanceof HTMLAnchorElement ?
+//          event.target.parentNode : null);
+
+//     // XXX only recognizes <a href="...">link</a> and <a
+//     // href="..."><img/></a>.
+//     if(htmlAnchor)
+//         openURL(htmlAnchor.href);
+// }
+
+
+// NETWORK REACTIONS
+// ----------------------------------------------------------------------
+
+// function sentChatActivation(message) {
+//     $('#conversations-box').collapsed = false;    
+// }
+
+
+// UTILITIES
+// ----------------------------------------------------------------------
+
+// function delayedMouseOver(xulElement, listener, delay) {
+//     delay = delay || 750;
+
+//     var timeout;
+//     xulElement.addEventListener('mouseover', function(event) {
+//         if(event.target != xulElement)
+//             return;
+//         timeout = setTimeout(listener, delay);
+//     }, false);
+
+//     xulElement.addEventListener('mouseout', function(event) {
+//         if(event.target != xulElement)
+//             return;
+//         clearTimeout(timeout);
+//     }, false);
+// }
