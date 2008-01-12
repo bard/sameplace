fd4eba987ad85a944d043a9b894aacde0ec1e39d
Massimiliano Mirra <bard@hyperstruct.net>
2008-01-12 12:25:57 +0100

    Preferences: removed pref to toggle conversation area.
    darcs-hash:20080112112557-8cc29-6837d24bf8317794076703f634bd6bbc9e70cba5.gz

--- defaults/preferences/sameplace.js
+++ defaults/preferences/sameplace.js
@@ -4,8 +4,6 @@ pref("extensions.sameplace.version", "");
 pref("extensions.sameplace.scriptlets.enabled", "[]");
 pref("extensions.sameplace.toggleContactsKey",
      "({ctrlKey:false, metaKey:false, shiftKey:false, altKey:false, charCode:0, keyCodeName:\"DOM_VK_F12\"})");
-pref("extensions.sameplace.toggleConversationsKey",
-     "({ctrlKey:false, metaKey:false, shiftKey:true, altKey:false, charCode:0, keyCodeName:\"DOM_VK_F12\"})");
 pref("extensions.sameplace.contactsArea", "left");
 pref("extensions.sameplace.conversationsArea", "left");
 pref("extensions.sameplace@hyperstruct.net.description", "chrome://sameplace/locale/sameplace.properties");
