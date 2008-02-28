be9a5575923d0db00643d7cd5a462b04e7aedacf
Massimiliano Mirra <bard@hyperstruct.net>
2008-02-28 10:32:19 +0100

    Draft Songbird compatibility.
    darcs-hash:20080228093219-8cc29-7beae80e2202bbcf68cd423f56892e86bd25b3de.gz

---
+++ chrome/content/overlay_songbird.js
@@ -0,0 +1,13 @@
+Cc['@songbirdnest.com/Songbird/DisplayPane/Manager;1']
+    .getService(Ci.sbIDisplayPaneManager)
+    .registerContent('chrome://sameplace/content/experimental/contacts.xul',
+                     'SamePlace',
+                     'chrome://sameplace/skin/logo16x16.png',
+                     300,
+                     400,
+                     'sidebar',
+                     true);
+
+
+
+
