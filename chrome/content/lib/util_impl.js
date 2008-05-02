91e035e5b545746a3e0999604f9b60b8860939cc
Massimiliano Mirra <bard@hyperstruct.net>
2008-05-02 14:33:33 +0200

    Chat app: indirected selection for app url, it is now defined in code instead of preferences.
    darcs-hash:20080502123333-8cc29-7c69021028587cce6f99777eb2c042ab1c387a36.gz

--- chrome/content/lib/util_impl.js
+++ chrome/content/lib/util_impl.js
@@ -109,6 +109,10 @@ function hostAppIsSongbird() {
 
 function getDefaultAppUrl() {
     var url = pref.getCharPref('defaultAppUrl');
+
+    if(url == 'default')
+        url = 'chrome://sameplace/content/app/chat.xhtml';
+
     if(/^chrome:\/\//.test(url) && !hostAppIsMail())
         // Thunderbird's content policy won't allow applications
         // served from file://.  For all others, we turn security up a
--- defaults/preferences/sameplace.js
+++ defaults/preferences/sameplace.js
@@ -1,4 +1,4 @@
-pref("extensions.sameplace.defaultAppUrl", "chrome://sameplace/content/app/chat.xhtml");
+pref("extensions.sameplace.defaultAppUrl", "default");
 pref("extensions.sameplace.getAttentionOnMessage", true);
 pref("extensions.sameplace.version", "");
 pref("extensions.sameplace.scriptlets.enabled", "[]");
