09a43fa2e77e124f0868f78be13fae8431d42ef4
Massimiliano Mirra <bard@hyperstruct.net>
2008-06-23 19:42:00 +0200

    About dialog: showing actual SamePlace version.
    darcs-hash:20080623174200-8cc29-0f0083b1059f7cc9594a6c398604acd4b8309a08.gz

---
+++ chrome/content/about.js
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2006-2007 by Massimiliano Mirra
+ * 
+ * This file is part of SamePlace.
+ * 
+ * SamePlace is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * SamePlace is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * The interactive user interfaces in modified source and object code
+ * versions of this program must display Appropriate Legal Notices, as
+ * required under Section 5 of the GNU General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU General Public License
+ * version 3, modified versions must display the "Powered by SamePlace"
+ * logo to users in a legible manner and the GPLv3 text must be made
+ * available to them.
+ * 
+ * Author: Massimiliano Mirra, <bard [at] hyperstruct [dot] net>
+ *  
+ */
+
+var Cc = Components.classes;
+var Ci = Components.interfaces;
+
+
+function init() {
+    document.getElementById('sameplace-version').value =
+        util.getExtensionVersion('sameplace@hyperstruct.net');
+    document.getElementById('xmpp4moz-version').value =
+        util.getExtensionVersion('xmpp4moz@hyperstruct.net');
+}
+
+function finish() {
+    
+}
