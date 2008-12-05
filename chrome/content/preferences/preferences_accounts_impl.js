c9751115bc9e4eaead8f5b7b3bce6e93996c9a85
Massimiliano Mirra <bard@hyperstruct.net>
2008-12-05 19:50:13 +0100

    Preferences: choked on empty window.arguments and did switch accounts.  Fixed.

--- chrome/content/preferences/preferences_accounts_impl.js
+++ chrome/content/preferences/preferences_accounts_impl.js
@@ -55,11 +55,13 @@ function init() {
     window.sizeToContent()
     refresh();
 
-    var id = window.arguments[1];
-    if(typeof(id) != 'undefined') {
-        var account = find(accounts, function(a) a.id == id);
-        $('#accounts').selectedItem = $('#accounts .account[id="' + id + '"]');
-        displayAccount(account);
+    if(window.arguments && window.arguments[1]) {
+        var id = window.arguments[1];
+        if(typeof(id) != 'undefined') {
+            var account = find(accounts, function(a) a.id == id);
+            $('#accounts').selectedItem = $('#accounts .account[id="' + id + '"]');
+            displayAccount(account);
+        }
     }
 }
 
@@ -137,7 +139,7 @@ function requestedAddAccount() {
 }
 
 function selectedAccount(event) {
-    var account = find(accounts, function(account) account.id = event.target.selectedItem.id)
+    var account = find(accounts, function(account) account.id == event.target.selectedItem.id)
     displayAccount(account);
 }
 
