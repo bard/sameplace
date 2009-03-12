f11b65b2180035056924f1a19caf09b72f9ca067
Massimiliano Mirra <bard@hyperstruct.net>
2009-03-12 18:19:34 +0100

    Contacts widget: avoid triggering the select event on the display mode radio if widget has not been initialized yet.

--- chrome/content/dashboard/widget_contacts.js
+++ chrome/content/dashboard/widget_contacts.js
@@ -333,6 +333,19 @@ contacts.requestedRenameContact = function(event, useNickAsPreset) {
     }
 };
 
+contacts.requestedChangeDisplayMode = function(event) {
+    if(this._displayMode === undefined)
+        // Veto the event.  If this._displayMode is not set, it means
+        // that we are being called before init() has a chance to run.
+        // This happens upon first load because of this chain of
+        // events: load event -> radio regains persisted value ->
+        // select event -> we are called -> dashboard/load event ->
+        // init() called -> this._displayMode gets initialize.
+        return;
+
+    this.changeDisplayMode(event.target.value);
+}
+
 
 // NETWORK REACTIONS
 // ----------------------------------------------------------------------
