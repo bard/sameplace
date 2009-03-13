57a48b9920c5c4757bf9266344e56db4d6d8a63f
Massimiliano Mirra <bard@hyperstruct.net>
2009-03-13 13:21:12 +0100

    Preferences: removed obsolete entries.

--- defaults/preferences/sameplace.js
+++ defaults/preferences/sameplace.js
@@ -1,9 +1,6 @@
 pref("extensions.sameplace.defaultAppUrl", "default");
 pref("extensions.sameplace.getAttentionOnMessage", true);
 pref("extensions.sameplace.version", "");
-pref("extensions.sameplace.scriptlets.enabled", "[]");
-pref("extensions.sameplace.toggleContactsKey",
-     "({ctrlKey:false, metaKey:false, shiftKey:false, altKey:false, charCode:0, keyCodeName:\"DOM_VK_F12\"})");
 pref("extensions.sameplace@hyperstruct.net.description", "chrome://sameplace/locale/sameplace.properties");
 pref("extensions.sameplace.branch", "devel");
 pref("extensions.sameplace.chatArea", "");
