69fb994540320756d2fc53c666a49a335aacb5b9
Massimiliano Mirra <bard@hyperstruct.net>
2009-03-14 12:35:13 +0100

    Stream: JSON not defined by default in Firefox 3.0, importing through xmpp4moz module.

--- chrome/content/stream/stream.js
+++ chrome/content/stream/stream.js
@@ -41,6 +41,7 @@ var Cu = Components.utils;
 Cu.import('resource://xmpp4moz/xmpp.jsm');
 Cu.import('resource://xmpp4moz/namespaces.jsm');
 Cu.import('resource://xmpp4moz/task.jsm');
+Cu.import('resource://xmpp4moz/json.jsm');
 Cu.import('resource://sameplace/main.jsm');
 
 
