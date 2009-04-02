273b09db3e5efe90e4ccd35b16d4fbc1818ec9e9
Massimiliano Mirra <bard@hyperstruct.net>
2009-04-03 01:40:17 +0200

    getElementsBySelector: Fixed memoization of css->xpath conversion (key was an empty string by the end of the function).

--- chrome/content/lib/getElementsBySelector.js
+++ chrome/content/lib/getElementsBySelector.js
@@ -56,119 +56,121 @@ function $$(first, second) {
     }
 }
 
-var cssToXPath = let(memo = {})
-    function(rule) {
-        if(memo[rule])
-            return memo[rule];
+function cssToXPath(rule) {
+    var memo = arguments.callee.memo || (arguments.callee.memo = {});
+
+    var origRule = rule;
+    if(origRule in memo)
+        return memo[origRule];
+    
+    var regElement = /^([#\.]?)([a-z0-9\\*_-]+)((\|)([a-z0-9\\*_-]*))?/i;
+    var regAttr1 = /^\[([^\]]*)\]/i;
+    var regAttr2 = /^\[\s*([^~=\s]+)\s*(~?=)\s*"([^\"]+)"\s*\]/i;
+    var regPseudo = /^:([a-z_-])+/i;
+    var regCombinator = /^(\s*[>^+\s-])?/i;
+    var regComma = /^\s*,/i;
+    
+    var index = 1;
+    var parts = rule.match(/^(\s*[>^+\s-])/i) ? ["./"] : [".//", "*"];
+    
+    var lastRule = null;
+    
+    while (rule.length && rule != lastRule)
+    {
+        lastRule = rule;
         
-        var regElement = /^([#\.]?)([a-z0-9\\*_-]+)((\|)([a-z0-9\\*_-]*))?/i;
-        var regAttr1 = /^\[([^\]]*)\]/i;
-        var regAttr2 = /^\[\s*([^~=\s]+)\s*(~?=)\s*"([^\"]+)"\s*\]/i;
-        var regPseudo = /^:([a-z_-])+/i;
-        var regCombinator = /^(\s*[>^+\s-])?/i;
-        var regComma = /^\s*,/i;
-        
-        var index = 1;
-        var parts = rule.match(/^(\s*[>^+\s-])/i) ? ["./"] : [".//", "*"];
-        
-        var lastRule = null;
-        
-        while (rule.length && rule != lastRule)
-        {
-            lastRule = rule;
-            
-            // Trim leading whitespace
-            rule = rule.replace(/^\s*|\s*$/g,"");
-            if (!rule.length)
+        // Trim leading whitespace
+        rule = rule.replace(/^\s*|\s*$/g,"");
+        if (!rule.length)
             break;
-            
-            // Match the element identifier
-            var m = regElement.exec(rule);
-            if (m)
+        
+        // Match the element identifier
+        var m = regElement.exec(rule);
+        if (m)
+        {
+            if (!m[1])
             {
-                if (!m[1])
-                {
-                    // XXXjoe Namespace ignored for now
-                    if (m[5])
+                // XXXjoe Namespace ignored for now
+                if (m[5])
                     parts[index] = m[5];
-                    else
+                else
                     parts[index] = '*[local-name() = "' + m[2] + '"]';
-                }
-                else if (m[1] == '#')
-                parts.push("[@id='" + m[2] + "']"); 
-                else if (m[1] == '.')
-                    parts.push("[contains(concat(' ', @class, ' '), ' " + m[2] + " ')]");
-                //parts.push("[contains(@class, '" + m[2] + "')]");               
-                rule = rule.substr(m[0].length);
             }
+            else if (m[1] == '#')
+                parts.push("[@id='" + m[2] + "']"); 
+            else if (m[1] == '.')
+                parts.push("[contains(concat(' ', @class, ' '), ' " + m[2] + " ')]");
+            //parts.push("[contains(@class, '" + m[2] + "')]");               
+            rule = rule.substr(m[0].length);
+        }
+        
+        // Match attribute selectors
+        m = regAttr2.exec(rule);
+        if (m)
+        {
+            if (m[2] == "~=")
+                parts.push("[contains(@" + m[1] + ", '" + m[3] + "')]");
+            else
+                parts.push("[@" + m[1] + "='" + m[3] + "']");
             
-            // Match attribute selectors
-            m = regAttr2.exec(rule);
+            rule = rule.substr(m[0].length);
+        }
+        else
+        {
+            m = regAttr1.exec(rule);
             if (m)
             {
-                if (m[2] == "~=")
-                parts.push("[contains(@" + m[1] + ", '" + m[3] + "')]");
-                else
-                parts.push("[@" + m[1] + "='" + m[3] + "']");
-                
+                parts.push("[@" + m[1] + "]");
                 rule = rule.substr(m[0].length);
             }
-            else
-            {
-                m = regAttr1.exec(rule);
-                if (m)
-                {
-                    parts.push("[@" + m[1] + "]");
-                    rule = rule.substr(m[0].length);
-                }
-            }
-            
-            // Skip over pseudo-classes and pseudo-elements, which are of no use to us
+        }
+        
+        // Skip over pseudo-classes and pseudo-elements, which are of no use to us
+        m = regPseudo.exec(rule);
+        while (m)
+        {
+            rule = rule.substr(m[0].length);
             m = regPseudo.exec(rule);
-            while (m)
-            {
-                rule = rule.substr(m[0].length);
-                m = regPseudo.exec(rule);
-            }
-            
-            // Match combinators
-            m = regCombinator.exec(rule);
-            if (m && m[0].length)
-            {
-                if (m[0].indexOf(">") != -1)
+        }
+        
+        // Match combinators
+        m = regCombinator.exec(rule);
+        if (m && m[0].length)
+        {
+            if (m[0].indexOf(">") != -1)
                 parts.push("/");
-                else if (m[0].indexOf("+") != -1)
+            else if (m[0].indexOf("+") != -1)
                 // XXXbard should probably be following-sibling[...][1]
                 parts.push("/following-sibling::");
-                else if (m[0].indexOf("^") != -1)
+            else if (m[0].indexOf("^") != -1)
                 parts.push("/ancestor::");
-                else if (m[0].indexOf("-") != -1)
+            else if (m[0].indexOf("-") != -1)
                 parts.push("/preceding-sibling::");
-                else
+            else
                 parts.push("//");
 
-                index = parts.length;
-                parts.push("*");
-                
-                rule = rule.substr(m[0].length);
-            }
+            index = parts.length;
+            parts.push("*");
             
-            m = regComma.exec(rule);
-            if (m)
-            {
-                // XXXbard maybe needs .//
-                parts.push(" | ", "//", "*");
-                index = parts.length-1;
-                rule = rule.substr(m[0].length);
-                
-            }
-
+            rule = rule.substr(m[0].length);
+        }
+        
+        m = regComma.exec(rule);
+        if (m)
+        {
+            // XXXbard maybe needs .//
+            parts.push(" | ", "//", "*");
+            index = parts.length-1;
+            rule = rule.substr(m[0].length);
+            
         }
 
-        var xpath = parts.join("");
-        memo[rule] = xpath;
-        return xpath;
     }
+
+    var xpath = parts.join("");
+    memo[origRule] = xpath;
+    return xpath;
+}
 
 cssToXPath.test = function() {
     if(!('assert' in this))
